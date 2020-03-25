80ece0ca735119be87f0e743e6c401389a4074ba
jhryu1208 <user-email>
2020-03-25 15:47:01 +0900

    JH_BMP_Code

---
+++ Book Management Program/Book Management Program/BMP.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#define NAME_SIZE 32
+#define BOOKNUMBER_SIZE 128
+
+struct _tagBook
+{
+	char	strBookname[NAME_SIZE];
+	char	strBookNum[BOOKNUMBER_SIZE];
+	int		iRent;
+	int		iPrice;
+	int		i
+};
